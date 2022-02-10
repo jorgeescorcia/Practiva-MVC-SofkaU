8dcbd198231104e547e88e75729504cbe9dded71
Jorge Andres Escorcia <jaescorcia10@gmail.com>
2022-02-10 13:36:35 -0500

    creamos una nueva clase Boardview para recibir el canva y creamos la funcion anonimas para las barras

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+
+//funcion anonima
 (function(){
 
     //funcion que nos crea en espacio de ping pong y le pasamos por parametros las dimensiones
-    var self_Board = function(width,height){
+    self.Board = function(width,height){
         this.width =width;
         this.height = height;
         this.playing = false;
@@ -11,20 +13,46 @@
 
     }
 
-    self_Board.prototype ={
+    self.Board.prototype ={
         get elements(){
 
             let elements = this.bars;
-            elements.push(ball)
+            elements.push(this.ball)
             return elements;
         }
     }
 
 {}})();
+//Funcion para crear las barras
+(function(){
+    //constructor de la barra donde le pasamos las dimensiones
+    self.Bar = function(x,y,width,height,board){
+        this.x =x;
+        this.y =y,
+        this.width = width;
+        this.height = height;
+        this.board = board;
+
+        this.board.bars.push(this);
+
+        this.kind = "rectangle";
+
+    }
+
+    self.Bar.prototype ={
+        //funciones para mover la barra
+        down: function(){
+
+        },
+        up: function(){
+
+        }
+    }
+})();
 
 
 (function(){
-    self_BoardView = function(canvas,board){
+    self.BoardView = function(canvas,board){
         this.canvas = canvas;
         this.canvas.width = board.width;
         this.canvas.height = board.height;
@@ -32,16 +60,45 @@
         this.ctx = canvas.getContext("2d");
 
     }
-})
+    self.BoardView.prototype={
+        draw: function(){
+            for (let i =  this.board.elements.length-1;i >= 0; i--) {
+                const elementos = this.board.elements[i];
+
+                draw(this.ctx,elementos);
+                
+            }
+        }
+    }
+    //Metodo que dibuja los elementos
+    function draw(ctx,element){
+        if(element != null && element.hasOwnProperty("kind")){
+
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x,element.y,element.width,element.height)
+                    break;
+            }
+    
+        }
+    
+    }
+
+})()
 
 window.addEventListener("load",main);
 
-/*Funcion principal donde hacemos el llamados de las funciones
- creadas anteriormente*/
+/*Funcion principal  que se muestra cuando se carga la ventana*/
 function main(){
-    let board = new self_Board(800,400)
-    let canvas = document.getElementsById("canvas")
-    let board_view = new self_BoardView(canvas,board)
+    let board = new Board(800,400)
+    var bar = new Bar(20,100,40,100,board)
+    var bar = new Bar(735,100,40,100,board)
+    let canvas = document.getElementById("canvas")
+    let board_view = new BoardView(canvas,board)
+
+    board_view.draw()
 
     
 }
+
+console.log("Holaaaa")
